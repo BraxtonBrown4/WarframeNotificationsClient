4b95eae68a47f346cf124337c1e06c59a048f3e3
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-18 13:05:15 -0500

    boilerplate routes for testing

---
+++ src/Invasions.tsx
@@ -0,0 +1,5 @@
+export const Invasions = () => {
+    return (
+        <h2>Invasions</h2>
+    )
+}
---
+++ src/NotificationForm.tsx
@@ -0,0 +1,8 @@
+export const NotificationForm = () => {
+  return (
+    <div>
+      <h2>Form</h2>
+      <button>Submit</button>
+    </div>
+  );
+};
