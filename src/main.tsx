033c5a8b804c515422e735cc36d00829ed1e3e68
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-22 03:49:55 -0500

    name change

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './index.css';
+import './main.css';
 import App from './App';
 
 const queryClient = new QueryClient();
