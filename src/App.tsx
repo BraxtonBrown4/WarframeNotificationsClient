8d24496ac6a5ac4a489038823ef7a38b53b12ea0
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-22 09:15:49 -0500

    changed outlet to fill

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Outlet, Route, Routes } from 'react-router-dom'
 import { NotificationForm } from './NotificationForm'
-import { Invasions } from './Invasions'
+import { Invasions } from './invasions/Invasions'
 import Navbar from './nav/Navbar'
 
 function App() {
   return <Routes>
-    <Route path="/" element={<><Navbar /> <div className="mt-[50px] px-4"> <Outlet /> </div></>}>
+    <Route path="/" element={<><Navbar /> <div className="mt-[50px] px-4 flex-grow"> <Outlet /> </div></>}>
       <Route path="/Invasions" element={<Invasions />} />
       <Route path="/Notification-Form" element={<NotificationForm />} />
     </Route>
