ff5b3b2577e219586839e80c2f7d54f50c1de118
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-22 05:49:37 -0500

    refactored navItem

--- src/nav/NavItem.tsx
+++ src/nav/NavItem.tsx
@@ -1,23 +1,26 @@
+import { Link, useLocation } from "react-router-dom";
+
 interface NavItemProps {
   icon: string;
-  id: number;
-  lockedId: number | null;
-  setLockedId: React.Dispatch<React.SetStateAction<number | null>>;
+  iconAlt: string;
+  URL: string;
+  linkTitle: string;
 }
 
-export default function NavItem({ icon, id, lockedId, setLockedId }: NavItemProps) {
+export default function NavItem({ icon, iconAlt, URL, linkTitle}: NavItemProps) {
+  const location = useLocation();
 
   return (
-    <div
-      className="relative group w-fit inline-block rounded overflow-hidden cursor-pointer"
-      onClick={() => {setLockedId(id)}}
-    >
-      <img src={icon} alt="Nav Icon" className="block w-[50px] h-[50px] object-contain" />
+    <Link to={URL} title={linkTitle} className="flex items-center justify-center">
       <div
-        className={`absolute inset-0 bg-red-500 transition-opacity ${
-          lockedId === id ? 'opacity-30' : 'opacity-0 group-hover:opacity-30'
-        }`}
-      />
-    </div>
+        className="relative group w-fit inline-block rounded overflow-hidden cursor-pointer">
+
+        <img src={icon} alt={iconAlt} className="block w-[50px] h-[50px] object-contain" />
+        <div
+          className={`absolute inset-0 bg-red-500 transition-opacity ${URL == location.pathname ? 'opacity-30' : 'opacity-0 group-hover:opacity-30'
+            }`}
+        />
+      </div>
+    </Link>
   );
 }
--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -1,21 +1,14 @@
-import { Link } from 'react-router-dom';
 import InvasionIcon from '../assets/InvasionIcon.webp';
 import NotificationTimerIcon from '../assets/NotificationTimerIcon.png';
-import { useState } from 'react';
 import NavItem from './NavItem';
 
 
 export default function Navbar() {
-  const [lockedId, setLockedId] = useState<number | null>(null);
-
+  
   return (
-    <nav className="bg-black text-white flex">
-      <Link to="/Invasions" title='Invasions' className="flex items-center justify-center">
-        <NavItem id={1} icon={InvasionIcon} lockedId={lockedId} setLockedId={setLockedId}/>
-      </Link>
-      <Link to="/Notification-Form" title='Notification Form' className="flex items-center justify-center">
-        <NavItem id={2} icon={NotificationTimerIcon} lockedId={lockedId} setLockedId={setLockedId}/>
-      </Link>
+    <nav className="bg-black w-screen h-[50px] text-white w flex fixed z-[9999]">
+        <NavItem icon={InvasionIcon} iconAlt='Invasion Icon' URL='/Invasions' linkTitle='Active Invasions' />
+        <NavItem icon={NotificationTimerIcon} iconAlt='Bell Icon' URL='/Notification-Form' linkTitle='Notification Form' />
     </nav>
   );
 }
