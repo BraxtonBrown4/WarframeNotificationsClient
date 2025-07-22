8785e482a73b4b9480d225f42ffe61cfcc95d758
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-22 05:49:58 -0500

    fixed navbar css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import { Outlet, Route, Routes } from 'react-router-dom'
 import { NotificationForm } from './NotificationForm'
 import { Invasions } from './Invasions'
 import Navbar from './nav/Navbar'
 
 function App() {
-  return <>
-    <Navbar />
-    <Routes>
+  return <Routes>
+    <Route path="/" element={<><Navbar /> <div className="mt-[50px] px-4"> <Outlet /> </div></>}>
       <Route path="/Invasions" element={<Invasions />} />
       <Route path="/Notification-Form" element={<NotificationForm />} />
-    </Routes>
-  </>
+    </Route>
+  </Routes>
 }
 
 export default App
