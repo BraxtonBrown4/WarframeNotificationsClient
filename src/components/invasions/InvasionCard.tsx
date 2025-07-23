8ba5fd6a43bf88b3027ed175e0bb61e2110e5bac
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-23 14:54:06 -0500

    won't run fetch if required item doesn't exist

--- src/components/invasions/InvasionCard.tsx
+++ src/components/invasions/InvasionCard.tsx
@@ -35,22 +35,24 @@ export default function InvasionCard({ invasion }: { invasion: Invasion }) {
 
     const { data: attackerReward, isLoading: DRIIsLoading } = useQuery<string>({
         queryKey: ['AttackerRewardImg', `${invasion.id}`],
-        queryFn: () => imgFinder(invasion?.attacker?.reward?.thumbnail)
+        queryFn: () => imgFinder(invasion?.attacker?.reward?.thumbnail),
+        enabled: !!invasion?.attacker?.reward?.thumbnail,
     })
 
     const { data: defenderReward, isLoading: ARIIsLoading } = useQuery<string>({
         queryKey: ['DefenderRewardImg', `${invasion.id}`],
-        queryFn: () => imgFinder(invasion?.defender?.reward?.thumbnail)
+        queryFn: () => imgFinder(invasion?.defender?.reward?.thumbnail),
+        enabled: !!invasion?.defender?.reward?.thumbnail
     });
 
-    return !invasion.completed && (
-        <div className="bg-gray-900 text-white font-mono text-sm border border-gray-500 w-full max-w-2xl m-4">
-            <div className="px-6 py-3 flex flex-col items-center">
-                <span className="font-bold text-gray-100 text-base">{invasion.node}: {invasion.desc}</span>
+    return (
+        <div className="bg-gray-900 text-white font-mono text-xs border border-gray-500 w-full max-w-md m-2">
+            <div className="px-3 py-2 flex flex-col items-center">
+                <span className="font-bold text-gray-100 text-sm">{invasion.node}: {invasion.desc}</span>
             </div>
 
-            <div className="flex flex-row justify-center items-center bg-gray-700 h-4 mb-5 overflow-hidden">
-                <h2 className="absolute">{Number(invasion.completion.toFixed(2))}% - Ends in: {invasion.eta}</h2>
+            <div className="flex flex-row justify-center items-center bg-gray-700 h-3 mb-3 overflow-hidden relative">
+                <h2 className="absolute text-xs">{Number(invasion.completion.toFixed(2))}% - Ends in: {invasion.eta}</h2>
                 <div
                     className={`right-0 top-0 h-full ${attackerInfo?.progressionColor}`}
                     style={{ width: `${invasion.completion}%` }}
@@ -60,35 +62,36 @@ export default function InvasionCard({ invasion }: { invasion: Invasion }) {
                 />
             </div>
 
-            <div className="px-6 py-4">
-                <div className="flex justify-between items-center mb-4">
-                    <div className="flex items-center space-x-3">
-                        <img src={attackerInfo?.icon} alt="Attacker Icon" className={`w-[40px] h-[40px] ${attackerInfo?.invertedIconColor}`} />
-                        <span className="text-gray-100 font-bold text-lg">{invasion.attacker.faction}</span>
+            <div className="px-3 py-2">
+                <div className="flex justify-between items-center mb-3">
+                    <div className="flex items-center space-x-2">
+                        <img src={attackerInfo?.icon} alt="Attacker Icon" className={`w-6 h-6 ${attackerInfo?.invertedIconColor}`} />
+                        <span className="text-gray-100 font-bold text-base">{invasion.attacker.faction}</span>
                     </div>
-                    <div className="flex items-center space-x-3">
-                        <span className="text-gray-100 font-bold text-lg">{invasion.defender.faction}</span>
-                        <img src={defenderInfo?.icon} alt="Defender Icon" className={`w-[40px] h-[40px] ${defenderInfo?.invertedIconColor}`} />
+                    <div className="flex items-center space-x-2">
+                        <span className="text-gray-100 font-bold text-base">{invasion.defender.faction}</span>
+                        <img src={defenderInfo?.icon} alt="Defender Icon" className={`w-6 h-6 ${defenderInfo?.invertedIconColor}`} />
                     </div>
                 </div>
-                <div className="flex justify-between items-center mb-4">
+                <div className="flex justify-between items-center mb-3">
 
-                    <div className="flex items-center space-x-3">
+                    <div className="flex items-center space-x-2">
                         {invasion.attacker.faction !== "Infested" && <>
-                            {!ARIIsLoading ? <img src={attackerReward} alt="Reward Image" className="w-[40px] h-[40px]" /> : <h2>Loading...</h2>}
-                            <span className="text-gray-100 font-bold text-lg">{invasion?.attacker?.reward?.asString}</span>
+                            {!ARIIsLoading ? <img src={attackerReward} alt="Reward Image" className="w-6 h-6" /> : <h2 className="text-xs">Loading...</h2>}
+                            <span className="text-gray-100 font-bold text-sm">{invasion?.attacker?.reward?.asString}</span>
                         </>}
                     </div>
 
-                    <div className="flex items-center space-x-3">
+                    <div className="flex items-center space-x-2">
                         {invasion.defender.faction !== "Infested" && <>
-                            <span className="text-gray-100 font-bold text-lg">{invasion?.defender?.reward?.asString}</span>
-                            {!DRIIsLoading ? <img src={defenderReward} alt="Reward Image" className="w-[40px] h-[40px]" /> : <h2>Loading...</h2>}
+                            <span className="text-gray-100 font-bold text-sm">{invasion?.defender?.reward?.asString}</span>
+                            {!DRIIsLoading ? <img src={defenderReward} alt="Reward Image" className="w-6 h-6" /> : <h2 className="text-xs">Loading...</h2>}
                         </>}
                     </div>
 
                 </div>
             </div>
         </div>
+
     );
 }
