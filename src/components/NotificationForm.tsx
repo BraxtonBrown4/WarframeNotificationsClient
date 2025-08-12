33f93dd142ed295cffbfc817951d9a096d7cc94b
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-08-12 16:52:39 -0500

    fixing cache fetching issue

--- src/components/NotificationForm.tsx
+++ src/components/NotificationForm.tsx
@@ -1,17 +1,16 @@
-import { QueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const NotificationForm = () => {
-  const queryClient = new QueryClient();
+  const queryClient = useQueryClient();
 
   const user = queryClient.getQueryData(["user"]);
-
 
   return (
     <div>
       <h2>Form</h2>
-      <button >Submit</button>
+      <button>Submit</button>
 
-      {!user &&
+      {!user && (
         <div className="fixed inset-0 flex items-center justify-center bg-white/20">
           <div className="bg-white rounded-lg p-6 max-w-sm w-full shadow-lg">
             <h2 className="text-lg flex justify-center items-center text-black font-semibold">
@@ -19,8 +18,7 @@ export const NotificationForm = () => {
             </h2>
           </div>
         </div>
-      }
+      )}
     </div>
-
   );
 };
