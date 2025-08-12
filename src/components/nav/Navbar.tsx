f47dbf78e0a3c8bc30953cd8ff820b360d4f9d81
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-08-12 16:28:03 -0500

    tailwind css

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -10,23 +10,31 @@ export default function Navbar() {
   const { data: user, isLoading } = useQuery({
     queryKey: ['User'],
     queryFn: () =>
-        fetch('/.auth/me', { credentials: 'include' })
-    .then(res => res.json())
-    .then(data => data.clientPrincipal || null),
+      fetch('/.auth/me', { credentials: 'include' })
+        .then(res => res.json())
+        .then(data => data.clientPrincipal || null),
     retry: false
-});
+  });
+
 
   return (
-    <nav className="bg-black w-screen h-[50px] text-white flex fixed z-50">
-      <NavItem icon={InvasionIcon} iconAlt='Invasion Icon' url='/Invasions' linkTitle='Active Invasions' />
-      <NavItem icon={VoidFissureIcon} iconAlt='Void Fissure Icon' url='/Void-Fissures' linkTitle='Active Void Fissures' />
-      <NavItem icon={SyndicateIcon} iconAlt='Syndicate Icon' url='/Syndicate-Missions' linkTitle='Syndicate Missions' />
-      <NavItem icon={NotificationTimerIcon} iconAlt='Bell Icon' url='/Notification-Form' linkTitle='Notification Form' />
+    <nav className="bg-black w-screen h-[50px] flex justify-between items-center fixed z-50 px-4">
 
-      {user ?
-      <button disabled = {isLoading} onClick={() => window.location.href = "/.auth/logout"}>Logout</button> :
-      <button disabled = {isLoading} onClick={() =>  window.location.href = "/.auth/login/aad"}>Login</button>
-      }
+        <NavItem icon={InvasionIcon} iconAlt='Invasion Icon' url='/Invasions' linkTitle='Active Invasions' />
+        <NavItem icon={VoidFissureIcon} iconAlt='Void Fissure Icon' url='/Void-Fissures' linkTitle='Active Void Fissures' />
+        <NavItem icon={SyndicateIcon} iconAlt='Syndicate Icon' url='/Syndicate-Missions' linkTitle='Syndicate Missions' />
+        <NavItem icon={NotificationTimerIcon} iconAlt='Bell Icon' url='/Notification-Form' linkTitle='Notification Form' />
+
+      <div className="ml-auto flex items-center pr-4">
+        {user ? (
+          <button disabled={isLoading} onClick={() => (window.location.href = "/.auth/logout")} className="!bg-red-500 text-black px-4 py-2 rounded disabled:opacity-50">
+            Logout
+          </button> ) : (
+          <button disabled={isLoading} onClick={() => (window.location.href = "/.auth/login/aad")} className="!bg-green-500 text-black px-4 py-2 rounded disabled:opacity-50">
+            Login
+          </button>
+        )}
+      </div>
     </nav>
   );
 }
